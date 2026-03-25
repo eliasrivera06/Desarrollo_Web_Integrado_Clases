836b452139fa5aaf42c9f119b7d8fcff9841c1c8
eliasrivera06 <eliasriverau06@gmail.com>
2026-03-25 15:39:16 -0500

    Primera clase

---
+++ appprueba/src/test/java/com/desarrolloweb/appprueba/ApppruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.appprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
